347ac283b3b745a6c77b7e658dbb9cd1efb5f33a
gtarrojo <175803244+gtarrojo@users.noreply.github.com>
2025-04-21 19:42:28 +0200

    add form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { FormComponent } from "./components/main/main.component";
+import { MainComponent } from "./components/main/main.component";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, FormComponent],
+  imports: [RouterOutlet, MainComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
+import { FormComponent } from "../form/form.component";
 
 @Component({
   selector: "app-main",
-  imports: [],
+  imports: [FormComponent],
   templateUrl: "./main.component.html",
   styleUrl: "./main.component.css",
 })
-export class FormComponent {}
+export class MainComponent {}
